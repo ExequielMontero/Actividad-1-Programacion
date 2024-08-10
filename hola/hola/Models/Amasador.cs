784000ce18cfdf9b04c3f404b75afaee76b5c31f
ExequielMontero <exeee2015@gmail.com>
2024-08-10 18:50:44 -0300

    Falto esto

---
+++ hola/hola/Models/Amasador.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hola.Models
+{
+    internal class Amasador
+    { 
+        public double amasar(double kgharina)
+        {
+            double harinaamasada = 0;
+            if (kgharina > 0)
+            {
+                harinaamasada = kgharina * 1.3;
+            }
+            return harinaamasada;
+        } 
+    }
+}
---
+++ hola/hola/Models/Cocinera.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hola.Models
+{
+    internal class Cocinera
+    {
+        public bool cortarmasa()
+        {
+            bool masa = false; ;
+            if(masa == false)
+            {
+                masa = true;
+            }
+            return masa;
+        }
+
+        public bool formarmolde()
+        {
+            bool molde = false; ;
+            if (molde == false)
+            {
+                molde = true;
+            }
+            return molde;
+        }
+
+        public bool hornear()
+        {
+            bool hornea = false;
+            if(hornea == false)
+            {
+                hornea = true;
+            }
+            return hornea;
+        }
+    }
+}
